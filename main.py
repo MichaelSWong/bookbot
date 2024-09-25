6ab368b122bd1024762041dee29bacec97d3d887
Michael S Wong <webdevlogics77@gmail.com>
2024-09-25 07:50:05 -0700

    changed to functions

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        words = file_contents.split()
-        count = 0
-        for word in words:
-            count+=1
-        print(count)
+    def print_words():
+        with open("books/frankenstein.txt") as f:
+            file_contents = f.read()
+            print(file_contents)
+    def word_count():
+        with open("books/frankenstein.txt") as f:
+            file_contents = f.read()
+            words = file_contents.split()
+            count = 0
+            for word in words:
+                count+=1
+            print(count)
+    print_words()
+    word_count()
 
 main()
